d6551311c8efc2bab154fdb7d0c6fb772ae706b0
Kelvin Chung <kelvincc@cs.washington.edu>
2019-07-21 08:04:13 -0700

    ffs

--- background.js
+++ background.js
@@ -1,10 +1,24 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+/*chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+  //console.log(tab);
+});*/
+
+/*
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {"message": "get_url"}, function(response) {
+    	console.log(response.yes);
+    });  
+});*/
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log(response.farewell);
   });
 });
--- content.js
+++ content.js
@@ -1,13 +1,23 @@
 //var firstHref = $("a[href^='http']").eq(0).attr("href");
 //console.log(firstHref);
-
+//alert(document.title);
 // content.js
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       console.log(firstHref);
+      console.log(sender);
     }
   }
-);
+);*/
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
