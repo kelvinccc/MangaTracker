ae9f2771e25dd191df167523cb46585317454a77
Kelvin Chung <kelvincc@cs.washington.edu>
2019-07-21 18:17:31 -0700

    Fixed merge conflicts

--- content.js
+++ content.js
@@ -1,33 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 //var firstHref = $("a[href^='http']").eq(0).attr("href");
 //console.log(firstHref);
-//alert(document.title);
+
 // content.js
-/*chrome.runtime.onMessage.addListener(
+chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       console.log(firstHref);
-      console.log(sender);
     }
   }
-);*/
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
-=======
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-console.log(firstHref);
->>>>>>> parent of 754bfeb... Added background script
-=======
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-console.log(firstHref);
->>>>>>> parent of 754bfeb... Added background script
+); 
