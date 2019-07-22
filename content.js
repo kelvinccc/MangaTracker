281e45b8f21cd6de9c32ef7966eb51dd58ca4428
Kelvin Chung <kelvincc@cs.washington.edu>
2019-07-21 19:33:06 -0700

    Added query function that grabs useful information
    title
    url
    hostname
    pathname

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
 //var firstHref = $("a[href^='http']").eq(0).attr("href");
 //console.log(firstHref);
 
+//Grab title of each webpage
+$(function() {
+  var title = $('title')[0].innerText;
+  var url = $(location).attr('href');
+  console.log(title);
+  console.log(url);
+  console.log(window.location.host); // can also use hostname
+  console.log(window.location.pathname);
+})
 // content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
