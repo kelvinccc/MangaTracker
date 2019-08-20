1d8e3daf20fcb7a5500a40d6cdefeab7793477b2
kelvinccc <kelvinc2016@gmail.com>
2019-08-21 02:17:55 +0800

    Display image and chapter onto MangaLibrary.html

--- mangaLibrary.js
+++ mangaLibrary.js
@@ -1,18 +1,17 @@
 var manga = [];
-
+var mangaKeys = [];
 chrome.storage.sync.get("manga", function(callback){
     var a = JSON.stringify(callback);
     console.log(a);
-    $('#manga').text(a);
     var mangas = callback.manga;
     console.log(mangas);
     //console.log('hi');
 
     //add each img of each manga into array manga
+    mangaKeys = Object.keys(mangas);
     for (var m in mangas) {
-    	console.log(mangas[m].img);
-    	manga.push(mangas[m].img);
-		//$( "#img" ).load( mangas[m].img + " div.manga-info-pic img" );
+    	console.log(mangas[m]);
+    	manga.push(mangas[m]);
 	}
 
     
@@ -20,9 +19,28 @@ chrome.storage.sync.get("manga", function(callback){
 
 // display each manga onto the screen
 $(function() {
-	manga.forEach(function(m) {
-		console.log(m);
-		$( "#img" ).load(m);
-	});
-})
+	//$('img').load("https://manganelo.com/manga/read_one_piece_manga_online_free4 div.manga-info-pic img");
 
+	for (i = 0; i < manga.length; i++) {
+		$("#manga").append(('<div id ="pic' + i + '"></div>'));
+		var len = manga[i].chapters.length;
+		var arr = manga[i].chapters.sort();
+		$("#manga").append('<p>' + mangaKeys[i] + " : " + manga[i].chapters[len - 1] + '</p>');
+		$('#pic' + i).load(manga[i].img);
+
+		$("#manga" + i).text(manga[i].chapters[len - 1]);
+	}
+
+	/*var $image = $("img").first();
+	var $downloadingImage = $("<img>");
+	$downloadingImage.load(function(){
+  		$image.attr("src", $(this).attr("src"));	
+	});
+	$downloadingImage.attr("src", "http://an.image/to/aynchrounously/download.jpg");*/
+});
+
+function loadImage(path, target) {
+    $(path).load(function() {
+      $(this).appendTo(target);
+    });
+}
